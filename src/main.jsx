d6ac75272d31eea85bb59bbc9e9fb3fcf25a1043
est3banst <estebeeen@outlook.com>
2025-02-17 01:41:01 -0300

    prob s c jajaja

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Nosotros from './components/AboutPage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
