e460e265a0c192694bc573627534a835753db0f9
est3banst <estebeeen@outlook.com>
2025-02-17 12:42:27 -0300

    d f me c her

--- src/components/GridHero.jsx
+++ src/components/GridHero.jsx
@@ -3,9 +3,12 @@ import React, { useState, useEffect } from "react";
 const GridHero = () => {
   const [numColumns, setNumColumns] = useState(0);
   const [numRows, setNumRows] = useState(0);
+  const [activeColumn, setActiveColumn] = useState(null);
+  const [key, setKey] = useState(0); 
 
   const squareSize = 1; 
   const gapSize = 40;   
+  const animationDuration = 4 * 1000; 
 
   const calcGrid = () => {
     const container = document.getElementById("hero-container");
@@ -30,9 +33,17 @@ const GridHero = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveColumn(Math.floor(Math.random() * numColumns));
+      setKey(prevKey => prevKey + 1);
+    }, animationDuration);
+
+    return () => clearInterval(interval);
+  }, [numColumns]);
+
   return (
-    <div
-      className="absolute z-20 top-0 left-0 w-full h-full grid"
+    <div className="absolute z-20 top-0 left-0 w-full h-full grid"
       style={{
         gridTemplateColumns: `repeat(${numColumns}, ${squareSize + gapSize}px)`,
         gridTemplateRows: `repeat(${numRows}, ${squareSize + gapSize}px)`,
@@ -40,9 +51,22 @@ const GridHero = () => {
     >
       {Array.from({ length: numColumns * numRows }).map((_, index) => (
         <div 
-        aria-hidden="true"
-        key={index} className="bg-[#171717] border-[.2px] border-[#a78bfa]" />
+          aria-hidden="true"
+          key={index} 
+          className="bg-[#171717] border-[.2px] border-[#a78bfa]" 
+        />
       ))}
+
+      {activeColumn !== null && (
+        <div 
+          key={key}
+          className="absolute top-0 left-0 w-[2.5px] h-full bg-[#a78bfa] opacity-50 animate-raydrop" 
+          style={{ 
+            left: `${activeColumn * (squareSize + gapSize)}px`,
+            animationDuration: "4s",
+          }}
+        />
+      )}
     </div>
   );
 };
