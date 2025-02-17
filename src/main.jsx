bef5b9ea52a685241ace402981e5eaf39a87d565
est3banst <estebeeen@outlook.com>
2025-02-17 01:32:53 -0300

    spa rtng

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx';
+import App from './App';
 import './index.css'
-import Nosotros from './components/AboutPage.jsx'; 
+import Nosotros from './components/AboutPage'; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/nosotros" element={<Nosotros />} /> 
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
 
