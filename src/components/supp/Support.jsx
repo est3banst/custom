b5a8f60fcf6a325f6ab91b5d044c4e89f70d30eb
est3banst <estebeeen@outlook.com>
2025-03-07 18:26:32 -0300

    all inkkkss v2

--- src/components/supp/Support.jsx
+++ src/components/supp/Support.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Support = () => {
   return (
-    <div>Support</div>
+    <>
+    <div className='container-conic'>
+        <div className='relative p-4'>
+        <section className='flex flex-col justify-center gap-2 leading-relaxed font-medium p-6'>
+            <h2>Tu web es lenta, no te trae clientes o no representa tu marca debido a que fue hecha hace mucho?</h2>
+            <hr />
+            <h2>Tenes una tienda virtual implementada que no cumple tus expectativas?</h2>
+            <hr />
+            <h2>Adquiriste un sistema para mejorar la gestión de tu negocio y en lugar de agilizar tus procesos te enlentece, debido a largos tiempos de espera por el soporte o simplemente un sistema lento?</h2>
+        </section>
+        <section className='bg-[#24242493] px-2 py-6'>
+            <h2 className='font-bold text-xl'>Somos tu socio ideal para que puedas usar la tecnología de manera eficiente, y no tengas que gastar montañas de dinero debido a recontratar una y otra vez para obtener lo mismo</h2>
+            <button className='action'>Comenza hoy</button>
+        </section>
+        </div>
+
+        <section className='relative w-full text-center underline py-4'>
+        <Link to="/">
+        Volver</Link>
+        </section>
+    </div>
+    </>
   )
 }
 
