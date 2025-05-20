1affb4a01724f2071eebb68345e6e9fb9083e66c
est3banst <estebeeen@outlook.com>
2025-05-19 22:49:14 -0300

    fix: v2 styling pages

--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -19,15 +19,15 @@ const ContactPage = () => {
       <Nav />
     </div>
 
-    <div className='grid relative grid-cols-1 md:grid-cols-2 lg:gap-2 gap-4 w-full'>
+    <div className='grid relative grid-cols-1 md:grid-cols-2 lg:gap-2 gap-2 w-full'>
         <div className="line topl"></div>
     <div className="line leftl"></div>
     <div className="line bottoml"></div>
     <div className="line rightl"></div>
-    <section className='flex w-full h-full justify-center items-center italiana px-6 py-12 flex-col gap-8'> 
-        <h2 className='font-bold relative z-[9999] md:text-xl border-b-2 border-[#a78bfa] py-2'>{lang === 'en' ? 'Looking for a personalized solution?': 'Buscas una solución personalizada?'}</h2>
-        <h2 className='font-bold relative z-[9999] text-center px-2 md:text-xl py-1'>{lang === 'en' ? 'Need a team ready to help you use technology to grow your business?' : 'Necesitas un equipo listo para ayudarte a utilizar la tecnología para hacer crecer tu negocio?'}</h2>
-        <p className='my-3 text-center relative z-[9999] leading-relaxed'>{lang === 'en' ? "You’re in the right place, leave us a message with your details, and we’ll get in touch with you!" : 'Estás en el lugar indicado, dejanos un mensaje con tus datos y nos pondremos en contacto contigo!'}</p>
+    <section className='flex max-w-2xl h-full justify-center items-center italiana px-6 py-12 flex-col gap-8'> 
+        <h2 className='font-bold relative md:text-xl border-b-2 border-[#a78bfa] py-2'>{lang === 'en' ? 'Looking for a personalized solution?': 'Buscas una solución personalizada?'}</h2>
+        <h2 className='font-bold relative  text-center px-4 md:text-xl py-1'>{lang === 'en' ? 'Need a team ready to help you use technology to grow your business?' : 'Necesitas un equipo listo para ayudarte a utilizar la tecnología para hacer crecer tu negocio?'}</h2>
+        <p className='my-3 px-4 text-center relative leading-relaxed'>{lang === 'en' ? "You’re in the right place, leave us a message with your details, and we’ll get in touch with you!" : 'Estás en el lugar indicado, dejanos un mensaje con tus datos y nos pondremos en contacto contigo!'}</p>
 
     </section>
    <div className='mb-4 flex justify-center'>
